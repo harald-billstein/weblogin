60e5949e6004e64d7b1ac5f70f0995f718f2357b
sdlonn <sdlonn@gmail.com>
2017-12-05 10:13:26 +0100

    Failing

--- src/com/weblogin/api/service/AuthorizationService.java
+++ src/com/weblogin/api/service/AuthorizationService.java
@@ -9,7 +9,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/api/login")
+@Path("/api")
 public class AuthorizationService extends AuthorizationImpl{
 
     @GET
